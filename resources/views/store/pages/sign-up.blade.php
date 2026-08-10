cc24abe793cd5c835e8fa8839a74ca0ac79387ac
riteshlahabar <riteshlahabar@gmail.com>
2026-08-10 14:33:42 +0530

    header bar logo

--- resources/views/store/pages/sign-up.blade.php
+++ resources/views/store/pages/sign-up.blade.php
@@ -21,6 +21,7 @@
 
     <!-- Template css -->
     <link id="color-link" rel="stylesheet" type="text/css" href="{{ asset('fastkart-store/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('fastkart-store/css/bawaskar-store.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
 

7ec733dcffda64137026e46ce2efd33f3803f69c
riteshlahabar <riteshlahabar@gmail.com>
2026-09-21 12:15:44 +0530

    updated the code for dealer and customer app

--- app/Http/Controllers/Admin/Dispatches/DispatchController.php
+++ app/Http/Controllers/Admin/Dispatches/DispatchController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers\Admin\Dispatches;
 
 use App\Http\Controllers\Admin\Concerns\AdminModuleController;
+use Illuminate\Http\Request;
 
 class DispatchController extends AdminModuleController
 {
     protected string $moduleKey = 'dispatches';
+
+    protected function prepareData(array $validated, Request $request, array $module): array
+    {
+        $data = parent::prepareData($validated, $request, $module);
+
+        if (empty($data['dispatch_no'])) {
+            $data['dispatch_no'] = 'DSP'.now()->format('ymdHis').random_int(100, 999);
+        }
+
+        return $data;
+    }
 }

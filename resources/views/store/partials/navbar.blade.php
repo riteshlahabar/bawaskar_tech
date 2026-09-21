d407435e5e415f4b53173a2d82bbd4419cadac88
riteshlahabar <riteshlahabar@gmail.com>
2026-09-21 17:21:41 +0530

    updated the code for dealer and customer app

--- resources/views/store/partials/navbar.blade.php
+++ resources/views/store/partials/navbar.blade.php
@@ -82,7 +82,7 @@
         <a class="nav-link dropdown-toggle" href="javascript:void(0)" data-bs-toggle="dropdown">{{ web_t('nav.mega_menu', 'Mega Menu') }}</a>
         <div class="dropdown-menu dropdown-menu-2 dropdown-menu-left">
             <div class="row">
-                <div class="dropdown-column col-xl-3">
+                <div class="dropdown-column col-xl-4">
                     <h5 class="dropdown-header">{{ web_t('nav.categories', 'Categories') }}</h5>
                     @forelse($navCategories->take(6) as $category)
                         <a class="dropdown-item" href="{{ $category->slug ? route('store.category', ['category' => $category->slug]) : $shopUrl }}">{{ $category->storefront_name }}</a>
@@ -90,13 +90,13 @@
                         <a class="dropdown-item" href="{{ $shopUrl }}">{{ web_t('nav.all_categories', 'All Categories') }}</a>
                     @endforelse
                 </div>
-                <div class="dropdown-column col-xl-3">
+                <div class="dropdown-column col-xl-4">
                     <h5 class="dropdown-header">Product Types</h5>
                     @foreach($navProductTypes->take(6) as $productType)
                         <a class="dropdown-item" href="{{ route('store.page', ['page' => 'shop-left-sidebar', 'product_type' => $productType['slug']]) }}">{{ storefront_public_t($productType['name'], 'product_type') }}</a>
                     @endforeach
                 </div>
-                <div class="dropdown-column col-xl-3">
+                <div class="dropdown-column col-xl-4">
                     <h5 class="dropdown-header">{{ web_t('nav.featured_products', 'Featured Products') }}</h5>
                     @forelse($navFeaturedProducts->take(6) as $product)
                         <a class="dropdown-item" href="{{ route('store.product', ['product' => $product->getKey()]) }}">{{ $product->translatedName() }}</a>
@@ -104,20 +104,23 @@
                         <a class="dropdown-item" href="{{ $shopUrl }}">{{ web_t('nav.all_products', 'All Products') }}</a>
                     @endforelse
                 </div>
-                <div class="dropdown-column col-xl-3">
-                    <h5 class="dropdown-header">{{ $storeUser ? 'My Account' : 'Customer' }}</h5>
-                    @if($storeUser)
-                        <a class="dropdown-item" href="{{ route('store.page', ['page' => 'user-dashboard']) }}">{{ web_t('nav.dashboard', 'Dashboard') }}</a>
-                        <a class="dropdown-item" href="{{ route('store.page', ['page' => 'user-dashboard']) }}#pills-order">{{ web_t('nav.recent_order', 'Recent Order') }}</a>
-                        <a class="dropdown-item" href="{{ route('store.page', ['page' => 'order-tracking']) }}">{{ web_t('nav.order_tracking', 'Order Tracking') }}</a>
-                    @else
-                        <a class="dropdown-item" href="{{ route('store.page', ['page' => 'login']) }}">{{ web_t('nav.customer_login', 'Customer Login') }}</a>
-                        <a class="dropdown-item" href="{{ route('store.page', ['page' => 'sign-up']) }}">{{ web_t('nav.customer_register', 'Customer Register') }}</a>
-                        <a class="dropdown-item" href="{{ route('store.page', ['page' => 'order-tracking']) }}">{{ web_t('nav.order_tracking', 'Order Tracking') }}</a>
-                    @endif
-                </div>
             </div>
         </div>
+    </li>
+
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="{{ $storeUser ? route('store.page', ['page' => 'user-dashboard']) : route('store.page', ['page' => 'login']) }}" data-bs-toggle="dropdown">{{ $storeUser ? 'My Account' : web_t('nav.customer', 'Customer') }}</a>
+        <ul class="dropdown-menu">
+            @if($storeUser)
+                <li><a class="dropdown-item" href="{{ route('store.page', ['page' => 'user-dashboard']) }}">{{ web_t('nav.dashboard', 'Dashboard') }}</a></li>
+                <li><a class="dropdown-item" href="{{ route('store.page', ['page' => 'user-dashboard']) }}#pills-order">{{ web_t('nav.recent_order', 'Recent Order') }}</a></li>
+                <li><a class="dropdown-item" href="{{ route('store.page', ['page' => 'order-tracking']) }}">{{ web_t('nav.order_tracking', 'Order Tracking') }}</a></li>
+            @else
+                <li><a class="dropdown-item" href="{{ route('store.page', ['page' => 'login']) }}">{{ web_t('nav.customer_login', 'Customer Login') }}</a></li>
+                <li><a class="dropdown-item" href="{{ route('store.page', ['page' => 'sign-up']) }}">{{ web_t('nav.customer_register', 'Customer Register') }}</a></li>
+                <li><a class="dropdown-item" href="{{ route('store.page', ['page' => 'order-tracking']) }}">{{ web_t('nav.order_tracking', 'Order Tracking') }}</a></li>
+            @endif
+        </ul>
     </li>
 
     <li class="nav-item dropdown">
@@ -140,11 +143,8 @@
         <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="{{ route('store.page', ['page' => 'about-us']) }}">{{ web_t('nav.company_profile', 'Company Profile') }}</a></li>
             <li><a class="dropdown-item" href="{{ route('store.page', ['page' => 'faq']) }}">{{ web_t('nav.faq', 'FAQ') }}</a></li>
+            <li><a class="dropdown-item" href="{{ route('store.page', ['page' => 'contact-us']) }}">{{ web_t('nav.contact_us', 'Contact Us') }}</a></li>
         </ul>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link no-dropdown-arrow" href="{{ route('store.page', ['page' => 'contact-us']) }}">{{ web_t('nav.contact_us', 'Contact Us') }}</a>
     </li>
 </ul>
 
